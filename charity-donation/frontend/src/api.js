8c19b27c07b637ab5c213b5f8e54d66dbbf79c8a
Dikosh <aldi12062006@gmail.com>
2026-02-09 06:05:35 +0500

    Update API base URL to production endpoint

--- charity-donation/frontend/src/api.js
+++ charity-donation/frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = "http://127.0.0.1:5050";
+export const API_BASE = "https://charity-donation-k1fx.onrender.com";
 
 export const api = axios.create({
   baseURL: API_BASE,
